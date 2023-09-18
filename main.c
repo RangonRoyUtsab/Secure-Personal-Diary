1b0a70c259001096b9612213b18a666a4c1e4c6a
rangon <rangonroy.rzs17@gmail.com>
2023-09-19 00:47:37 +0600

    added visual improvement

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <time.h>
 
-#include "diary.h"
-#include "hash.h"      // contains the hashing functions
-#include "misc.h"      // contains delay() & show_Time()
-#include "password.h"  //contains setpass() & password_change()
+#include "diary.h"    //contains diary functions
+#include "hash.h"     // contains the hashing functions
+#include "misc.h"     // contains delay() & show_Time()
+#include "password.h" //contains setpass() & password_change()
 #include "test_header.h"
 
 #define DATE_SIZE 50
@@ -18,182 +18,180 @@ typedef long long LL;
 
 int user_level = 0;
 
-void menu() {
+void menu () {
   int flag = 1, choice;
   char ch;
   while (flag) {
-    system("cls");
-    show_Time();
-    printf("Current User: ");
+    system ("cls");
+    show_Time ();
+    printf ("Current User: ");
     switch (user_level) {
-      case 0:
-        printf("Guest\n");
-        break;
-      case 1:
-        printf("Life Partner\n");
-        break;
-      case 2:
-        printf("Admin\n");
-        break;
+    case 0: printf ("Guest\n"); break;
+    case 1: printf ("Life Partner\n"); break;
+    case 2: printf ("Admin\n"); break;
     }
-    printf("\n");
-    printf(">> MENU << \n");
-    printf("1. Add Record\n");
-    printf("2. View Record\n");
-    printf("3. Modify Record\n");
-    printf("4. Delete Record\n");
-    printf("5. Change Password\n");
-    printf("6. Log Out\n");
-    printf(">> Enter your choice: ");
+    printf ("*******************************************************************************\n");
+    printf ("\n");
+    printf (">> MENU << \n");
+    printf ("1. Add Record\n");
+    printf ("2. View Record\n");
+    printf ("3. Modify Record\n");
+    printf ("4. Delete Record\n");
+    printf ("5. Change Password\n");
+    printf ("6. Log Out\n");
+    printf (">> Enter your choice: ");
 
-    ch = _getch();
+    ch     = _getch ();
     choice = ch - '0';
-    printf("%d\n", choice);
+    printf ("%d\n", choice);
     // delay(1);
     switch (choice) {
-      case 1: {
-        add_record(user_level);
-        printf("Press any key to continue.\n");
-        getch();
-        break;
-      }
-      case 2: {
-        view_record(user_level);
-        printf("Press any key to continue.\n");
-        getch();
-        break;
-      }
-      case 3: {
-        modify_record(user_level);
-        printf("Press any key to continue.\n");
-        getch();
-        break;
-      }
-      case 4: {
-        delete_record(user_level);
-        printf("Press any key to continue.\n");
-        getch();
-        break;
-      }
-      case 5: {
-        password_change(user_level);
-        printf("Press any key to continue.\n");
-        getch();
-        break;
-      }
-      case 6: {
-        flag = 0;
-        user_level = 0;
-        printf("Press any key to continue.\n");
-        break;
-      }
-      default: {
-        printf("Please enter a valid choice\n");
-        delay(1);
-        break;
-      }
+    case 1: {
+      add_record (user_level);
+      printf ("Press any key to continue.\n");
+      getch ();
+      break;
+    }
+    case 2: {
+      view_record (user_level);
+      printf ("Press any key to continue.\n");
+      getch ();
+      break;
+    }
+    case 3: {
+      modify_record (user_level);
+      printf ("Press any key to continue.\n");
+      getch ();
+      break;
+    }
+    case 4: {
+      delete_record (user_level);
+      printf ("Press any key to continue.\n");
+      getch ();
+      break;
+    }
+    case 5: {
+      password_change (user_level);
+      printf ("Press any key to continue.\n");
+      getch ();
+      break;
+    }
+    case 6: {
+      flag       = 0;
+      user_level = 0;
+      printf ("Logging out...\n");
+      delay (1);
+      break;
+    }
+    default: {
+      printf ("Please enter a valid choice.\n");
+      delay (1);
+      break;
+    }
     }
   }
 }
 
-int main() {
+int main () {
   int f = 1;
 again:
   while (f) {
-    system("cls");
+    system ("cls");
     char s[100];
     char ch;
     int choice, j;
     LL a, b;
 
-    printf(">> WELCOME TO YOUR SECURE PERSONAL DIARY\n\n");
-    printf(">> LOGIN MENU: \n\n");
+    printf ("*******************************************************************************\n");
+    printf ("******************** WELCOME TO YOUR SECURE PERSONAL DIARY ********************\n");
+    printf ("*******************************************************************************\n");
+    printf ("*                                                                             *\n");
+    printf ("*                              LOGIN MENU                                     *\n");
+    printf ("* SELECT USER:                                                                *\n");
+    printf ("* 1. Admin                                                                    *\n");
+    printf ("* 2. Life Partner                                                             *\n");
+    printf ("* 3. Guest                                                                    *\n");
+    printf ("* 4. Exit                                                                     *\n");
 
-    printf(">> SELECT USER: \n");
-    printf(">> 1. Admin\n");
-    printf(">> 2. Life Partner\n");
-    printf(">> 3. Guest\n");
-    printf(">> 4. Exit\n");
-
-    printf(">> Choice: ");
-    ch = _getch();
+    printf ("* Choice: ");
+    ch     = _getch ();
     choice = ch - '0';
-    printf("%d\n", choice);
+    printf ("%d\n", choice);
     // delay(1);
 
     switch (choice) {
-      case 1:
-        user_level = 2;
-        break;
-      case 2:
-        user_level = 1;
-        break;
-      case 3:
-        user_level = 0;
-        break;
-      case 4:
-        exit(1);
-      default:
-        printf("Invalid Choice.\n");
-        delay(1);
-        goto again;
+    case 1: user_level = 2; break;
+    case 2: user_level = 1; break;
+    case 3: user_level = 0; break;
+    case 4:
+      printf ("                           *** Exiting ...***\n");
+      delay (1);
+      exit (1);
+    default:
+      printf ("                          *** Invalid Choice ***\n");
+      delay (1);
+      goto again;
     }
 
     if (user_level == 0) {
-      menu();
+      menu ();
     }
 
     else {
-      FILE *fp;
+      FILE* fp;
 
       if (user_level == 2) {
-        fp = fopen("admin_pass", "r");
+        fp = fopen ("admin_pass", "r");
 
         if (fp == NULL) {
-          setpass(user_level);
-          printf(">> Press any key to continue");
-          getch();
+          setpass (user_level);
+          printf ("                     *** Press any key to continue ***");
+          getch ();
           continue;
         }
       }
       if (user_level == 1) {
-        fp = fopen("part_pass", "r");
+        fp = fopen ("part_pass", "r");
 
         if (fp == NULL) {
-          setpass(user_level);
-          printf(">> Press any key to continue");
-          getch();
+          setpass (user_level);
+          printf ("                     *** Press any key to continue ***");
+          getch ();
           continue;
         }
       }
 
-      printf(">> ENTER PASSWORD (Type \"q\" to exit): ");
-      fflush(stdin);
+      printf ("* ENTER PASSWORD (Enter \"q\" to exit): ");
+      fflush (stdin);
 
       j = 0;
-      while ((ch = _getch()) != 13)  // character 13 is enter
+      while ((ch = _getch ()) != 13) // character 13 is enter
       {
-        if (ch == 8) continue;
+        if (ch == 8)
+          continue;
         s[j] = ch;
         j++;
-        printf("*");
+        printf ("*");
       }
       s[j] = '\0';
-      printf("\n");
+      printf ("\n");
 
-      if (strcmp(s, "q") == 0) break;
+      if (strcmp (s, "q") == 0)
+        break;
 
-      a = HASH(s);
-      fread(&b, sizeof(b), 1, fp);
-      fclose(fp);
+      a = HASH (s);
+      fread (&b, sizeof (b), 1, fp);
+      fclose (fp);
 
       if (a == b) {
-        printf(">> Password matched!\n");
-        delay(1);
-        menu();
+        printf ("                         *** Password matched ***\n");
+        printf ("                           *** Signing in...***\n");
+        delay (1);
+        menu ();
       } else {
-        printf("!! Wrong password. Try again.\n");
-        delay(1);
+        printf ("                          *** Wrong password ***\n");
+        printf ("                            *** Try Again ***\n");
+        delay (1);
       }
     }
   }
